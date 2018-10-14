959a4dc4436a4f6fe5352b3bc229f0bc1fa689aa
Unknown <218435@student.pwr.edu.pl>
2018-10-14 20:03:52 +0200

    Utworzenie projektu

---
+++ src/main/java/com/example/trainingplan/TrainingPlanApplication.java
@@ -0,0 +1,12 @@
+package com.example.trainingplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingPlanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingPlanApplication.class, args);
+    }
+}
