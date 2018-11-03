a62f383168fedc573fadfec3c81f4cd0c2f09327
Unknown <218435@student.pwr.edu.pl>
2018-11-03 15:17:09 +0100

    Zalogowany uzytkownik
    - pobranie loginu zalogowane uzytkowanika

--- src/main/java/com/example/trainingplan/controllers/HomeController.java
+++ src/main/java/com/example/trainingplan/controllers/HomeController.java
@@ -4,12 +4,15 @@ package com.example.trainingplan.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.security.Principal;
+
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home(){
+    public String home(Principal principal){
+        System.out.println(principal.getName());
         return "index";
     }
 }
