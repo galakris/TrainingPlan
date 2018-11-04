818561bd7001d7f0dbb9ae35c889ccf046582ec7
Unknown <218435@student.pwr.edu.pl>
2018-11-04 15:08:03 +0100

    Informacja o zalogowanym uzytkowniku

--- src/main/java/com/example/trainingplan/controllers/HomeController.java
+++ src/main/java/com/example/trainingplan/controllers/HomeController.java
@@ -1,7 +1,10 @@
 package com.example.trainingplan.controllers;
 
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.security.Principal;
@@ -11,8 +14,13 @@ import java.security.Principal;
 public class HomeController {
 
     @GetMapping("/")
-    public String home(Principal principal){
+    public String home(Principal principal, Model model){
         System.out.println(principal.getName());
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println(auth.getName());
+        model.addAttribute("name", auth.getName());
+
         return "index";
     }
 
